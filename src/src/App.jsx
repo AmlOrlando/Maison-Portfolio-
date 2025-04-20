f8480915bafd52c67c1deb3d9db2cb901e5e7ded
AmlOrlando <Adrianmunch77@gmail.com>
2025-04-20 14:13:58 +0200

    Create App.jsx

---
+++ src/src/App.jsx
@@ -0,0 +1,34 @@
+import { ThemeProvider } from './context/ThemeContext'
+import Header from './components/layout/Header'
+import Footer from './components/layout/Footer'
+import Hero from './components/sections/Hero'
+import About from './components/sections/About'
+import Services from './components/sections/Services'
+import Projects from './components/sections/Projects'
+import Testimonials from './components/sections/Testimonials'
+import Contact from './components/sections/Contact'
+import FAQ from './components/sections/FAQ'
+import Insights from './components/sections/Insights'
+
+function App() {
+  return (
+    <ThemeProvider>
+      <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300">
+        <Header />
+        <main>
+          <Hero />
+          <About />
+          <Services />
+          <Projects />
+          <Testimonials />
+          <Insights />
+          <FAQ />
+          <Contact />
+        </main>
+        <Footer />
+      </div>
+    </ThemeProvider>
+  )
+}
+
+export default App
